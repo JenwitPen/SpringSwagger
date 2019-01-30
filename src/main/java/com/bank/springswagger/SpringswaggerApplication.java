8c8d6a3df0161dbb1f92c795f021b29482935849
Jenwit Penjamrat <jenwit_penjamrat@hotmail.com>
2019-01-30 10:42:07 +0700

    Initial Poject

---
+++ src/main/java/com/bank/springswagger/SpringswaggerApplication.java
@@ -0,0 +1,14 @@
+package com.bank.springswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringswaggerApplication.class, args);
+	}
+
+}
+
