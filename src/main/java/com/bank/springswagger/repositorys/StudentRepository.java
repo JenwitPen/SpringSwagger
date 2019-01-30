101ab7954312519de048c068faa74e878ae09807
Jenwit <jenwit_penjamrat@hotmail.com>
2019-01-30 14:07:58 +0700

    add h2 database add JPA

---
+++ src/main/java/com/bank/springswagger/repositorys/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.bank.springswagger.repositorys;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bank.springswagger.models.Student;
+
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
